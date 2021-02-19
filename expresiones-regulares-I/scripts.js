fbb0d0490438427fa311ea2751b4514d5e30df2b
Andres Bertone <andresbertone97@gmail.com>
2021-02-19 14:47:50 -0300

    Comienzo de expresiones regulares para validar

---
+++ expresiones-regulares-I/scripts.js
@@ -0,0 +1,18 @@
+/*
+    Expresiones regulares:
+        Son una secuencia de caracteres que forma un patrón de búsqueda, principalmente utilizada para la búsqueda de patrones de cadenas de caracteres u operaciones de sustituciones
+    
+        Sintaxis:
+            /<patron>/<bandera>
+
+        Banderas:
+        i: ignore case. No diferencia entre mayusculas y minusculas
+        
+        g: global. Busca de forma global, es decir, no se para después de la primera coincidencia
+*/
+
+const text = document.getElementById('text').textContent;
+
+const regEx = /lorem/gi;
+
+console.log(regEx.test(text))
